fb86e89d484191029c2e4627993686324fc03956
Emile <e.van.krieken@vu.nl>
2021-01-28 21:26:13 +0100

    Context menu search

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {EventRef, MarkdownPreviewView, MarkdownView, Plugin, TFile} from 'obsidian';
+import {EventRef, MarkdownPreviewView, MarkdownView, Menu, Plugin, TFile} from 'obsidian';
 import {SOISettingTab, SOISettings, DEFAULT_SETTING, SearchSetting} from './settings';
 import open from 'open';
 import {SearchModal} from './modal';
@@ -39,14 +39,8 @@ export default class SearchOnInternetPlugin extends Plugin {
         id: 'search-on-internet',
         name: 'Perform search',
         callback: () => {
-          let query = null;
-          const wSelection = window.getSelection();
-          const docSelection = document?.getSelection();
-          if (wSelection) {
-            query = wSelection.toString();
-          } else if (document && docSelection.type != 'Control') {
-            query = docSelection.toString();
-          }
+          let query = this.getSelectedText();
+
           if (query === null || query === '') {
             const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
             if (activeView == null) {
@@ -65,24 +59,64 @@ export default class SearchOnInternetPlugin extends Plugin {
       // on internal link. But other times, it's a context menu that
       // cannot really be edited easily. It would be nice if Obsidian
       // provided its own context menu everywhere to hook into.
-      // this.registerCodeMirror((cm) => {
-      //   // @ts-ignore
-      //   cm.resetSelectionOnContextMenu=false;
-      //   cm.on('contextmenu', (editor, event)=>{
-      //     console.log(editor);
-      //     console.log(event);
-      //   });
-      // });
+
+      this.registerCodeMirror((cm) => {
+        // @ts-ignore
+        cm.resetSelectionOnContextMenu=false;
+        cm.on('contextmenu', (editor, event)=>{
+          console.log(editor);
+          console.log(event);
+          this.handleContext(event);
+        });
+      });
+      document.on('contextmenu', '.markdown-preview-view', (event) => {
+        console.log(event);
+        this.handleContext(event);
+      });
     }
 
-    async openSearch(search: SearchSetting, query: string) {
+    getSelectedText(): string {
+      const wSelection = window.getSelection();
+      const docSelection = document?.getSelection();
+      if (wSelection) {
+        return wSelection.toString();
+      } else if (document && docSelection.type != 'Control') {
+        return docSelection.toString();
+      }
+      return null;
+    }
+
+    async handleContext(e: MouseEvent, activeView: SearchView=null) {
+      const query = this.getSelectedText();
+      if (query === null || query === '') {
+        return;
+      }
+      const fileMenu = new Menu();
+      for (const setting of this.settings.searches) {
+        fileMenu.addItem((item) => {
+          item.setTitle(`Search ${setting.name}`).setIcon('search')
+              .onClick((evt) => {
+                this.openSearch(setting, query, activeView);
+              });
+        });
+      }
+      fileMenu.showAtPosition({x: e.x, y: e.y});
+      e.preventDefault();
+    }
+
+    async openSearch(search: SearchSetting, query: string, activeView: SearchView=null) {
       const url = search.query.replace('{{title}}', encodeURIComponent(query));
       console.log(`SOI: Opening URL ${url}`);
       if (this.settings.useIframe) {
-        const leaf = this.app.workspace.getLeaf(!(this.app.workspace.activeLeaf.view.getViewType() === 'empty'));
-        // const leaf = this.app.workspace.splitActiveLeaf(this.settings.splitDirection);
-        const view = new SearchView(leaf, query, search.name, url);
-        await leaf.open(view);
+        if (activeView) {
+          activeView.frame.setAttr('src', url);
+          activeView.url = url;
+        } else {
+          const leaf = this.app.workspace.getLeaf(!(this.app.workspace.activeLeaf.view.getViewType() === 'empty'));
+          // const leaf = this.app.workspace.splitActiveLeaf(this.settings.splitDirection);
+          const view = new SearchView(this, leaf, query, search.name, url);
+          await leaf.open(view);
+        }
       } else {
         await open(url);
       }
--- view.ts
+++ view.ts
@@ -1,24 +1,36 @@
 import {ItemView, WorkspaceLeaf} from 'obsidian';
+import SearchOnInternetPlugin from './main';
 
 export class SearchView extends ItemView {
     query: string;
     site: string;
     url: string;
+    plugin: SearchOnInternetPlugin;
 
-    constructor(leaf: WorkspaceLeaf, query: string, site: string, url: string) {
+    frame: HTMLElement;
+
+    constructor(plugin: SearchOnInternetPlugin, leaf: WorkspaceLeaf, query: string, site: string, url: string) {
       super(leaf);
       this.query= query;
       this.site = site;
       this.url = url;
+      this.plugin = plugin;
     }
 
     async onOpen() {
-      const frame = document.createElement('iframe');
-      frame.addClass(`soi-site`);
-      frame.setAttr('style', 'height: 100%; width:100%');
-      frame.setAttr('src', this.url);
-      frame.setAttr('tabindex', '0');
-      this.containerEl.children[1].appendChild(frame);
+      this.frame = document.createElement('iframe');
+      this.frame.addClass(`soi-site`);
+      this.frame.setAttr('style', 'height: 100%; width:100%');
+      this.frame.setAttr('src', this.url);
+      this.frame.setAttr('tabindex', '0');
+      this.containerEl.children[1].appendChild(this.frame);
+
+
+      // Turns out IFrames are very hard to control the contextmenu of. So leaving this for now!
+      // this.frame.addEventListener('contextmenu', (e) => {
+      //   console.log('asdf');
+      //   this.plugin.handleContext(e, this);
+      // });
     }
 
     getDisplayText(): string {
